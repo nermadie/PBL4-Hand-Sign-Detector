581c065415d2c4c7917b13313172e28b120d3d94
TranNhatMinh <nhatminh10b1@gmail.com>
2023-07-07 07:54:50 +0700

    PBL4: Project done

---
+++ Code/Clone/dataCollection.py
@@ -0,0 +1,50 @@
+import cv2
+from cvzone.HandTrackingModule import HandDetector
+import numpy as np
+import math
+import time
+
+cap = cv2.VideoCapture(0)
+detector = HandDetector(maxHands=1)
+
+offset = 20
+imgSize = 300
+
+folder = "Data/A"
+
+while True:
+    success, img = cap.read()
+    hands, img = detector.findHands(img)
+    if hands:
+        hand = hands[0]
+        x, y, w, h = hand['bbox']
+
+        imgWhite = np.ones((imgSize, imgSize, 3), np.uint8)
+
+        imgCrop = img[(y-offset) if (y-offset) >= 0 else 0:y+h +
+                      offset, (x-offset) if (x-offset) >= 0 else 0:x+w+offset]
+
+        aspectRatio = h/w
+        if aspectRatio > 1:
+            k = imgSize/h
+            wCal = math.ceil(k*w)
+            imgResize = cv2.resize(imgCrop, (wCal, imgSize))
+            imgResizeShape = imgResize.shape
+            wGap = math.ceil((imgSize-wCal)/2)
+            imgWhite[:, wGap:wCal+wGap] = imgResize
+        else:
+            k = imgSize/w
+            hCal = math.ceil(k*h)
+            imgResize = cv2.resize(imgCrop, (imgSize, hCal))
+            imgResizeShape = imgResize.shape
+            hGap = math.ceil((imgSize-hCal)/2)
+            imgWhite[hGap:hCal+hGap, :] = imgResize
+
+        cv2.imshow("ImageCrop", imgCrop)
+        cv2.imshow("ImageWhite", imgWhite)
+    cv2.imshow("Image", img)
+    cv2.waitKey(20)
+    if key = ord("s"):
+        counter += 1
+        cv2.imwrite(f'{folder}/{time.time()}.png', imgWhite)
+        print(counter)
